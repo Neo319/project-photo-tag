81a438b02ea5e400cb5850f3959985caf608b744
CastorDisaster <alexmnevins@gmail.com>
2024-11-10 13:16:54 -0500

    render target div in approppriate location

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,15 @@
 console.log("hello world");
 
 // mock db
-const imageLink = "../src/91qfVY7.jpeg";
+const imgData = {
+  url: "../src/91qfVY7.jpeg",
+  locations: {
+    Waldo: {
+      x: 63.6,
+      y: 50.3,
+    },
+  },
+};
 
 // DOM pieces
 const mainDiv = document.getElementById("main");
@@ -11,17 +19,20 @@ function loadImage(url) {
   img.src = url;
 }
 // reveal Waldo's location in the image with given pixel coordinates
-function createTarget(x, y) {
+function createTarget(locations) {
   const target = document.createElement("div");
   target.classList = "target";
 
-  target.style.left = "10%";
-  target.style.top = "10%";
+  //temp: assume only 1 location
+  const { x, y } = locations.Waldo;
+
+  target.style.left = `${x}%`;
+  target.style.top = `${y}%`;
 
   mainDiv.appendChild(target);
 }
 
-loadImage(imageLink);
+loadImage(imgData.url);
 console.log("loaded image.");
 
-createTarget(10, 10);
+createTarget(imgData.locations);
