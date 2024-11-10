60ace904a045b751e15aad0de1eb7c7abb823ab2
CastorDisaster <alexmnevins@gmail.com>
2024-11-10 12:44:52 -0500

    create div for targeting

--- docs/script.js
+++ docs/script.js
@@ -10,6 +10,18 @@ const img = document.querySelector("img");
 function loadImage(url) {
   img.src = url;
 }
+// reveal Waldo's location in the image with given pixel coordinates
+function createTarget(x, y) {
+  const target = document.createElement("div");
+  target.classList = "target";
+
+  target.style.left = "10%";
+  target.style.top = "10%";
+
+  mainDiv.appendChild(target);
+}
 
 loadImage(imageLink);
-console.log("loaded image.(?)");
+console.log("loaded image.");
+
+createTarget(10, 10);
