a5fc21b6b2a9797a8b6a094daeeb58b0f56ba479
CastorDisaster <alexmnevins@gmail.com>
2024-11-10 14:43:00 -0500

    create dropdown menu on click

--- docs/scriptRW.js
+++ docs/scriptRW.js
@@ -3,11 +3,12 @@ console.log("hello world");
 // mock db
 const imgData = {
   url: "../src/91qfVY7.jpeg",
-  locations: {
-    Waldo: {
+  locations: [
+    {
+      name: "Waldo",
       // TBD
     },
-  },
+  ],
 };
 
 // DOM pieces
@@ -17,6 +18,19 @@ const img = document.querySelector("img");
 const targetBox = document.createElement("div"); // to be used later
 targetBox.id = "targetBox";
 targetBox.hidden = true;
+
+const dropdown = document.createElement("select"); // to be used later
+dropdown.id = "dropdown";
+dropdown.hidden = true;
+
+const createDropDownOptions = (() => {
+  dropdown.add(new Option("Which character is here? Choose one:", false, true));
+
+  //create locations based on imgData from db
+  imgData.locations.map((location) => {
+    dropdown.add(new Option(location.name));
+  });
+})();
 
 function loadImage(url) {
   img.src = url;
@@ -33,9 +47,21 @@ function createTarget(e) {
   mainDiv.appendChild(targetBox);
 }
 
+function createDropDown(e) {
+  // get click location in px
+  const { offsetX: x, offsetY: y } = e;
+
+  dropdown.style.top = `${y}px`;
+  dropdown.style.left = `${x}px`;
+  dropdown.hidden = false;
+
+  mainDiv.appendChild(dropdown);
+}
+
 loadImage(imgData.url);
 
 // add a target box, and add a dropdown menu
 mainDiv.addEventListener("click", (e) => {
   createTarget(e);
+  createDropDown(e);
 });
