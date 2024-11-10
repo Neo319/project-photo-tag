21ab1a16fe35757c1a83f571bb389de66a683410
CastorDisaster <alexmnevins@gmail.com>
2024-11-10 12:18:49 -0500

    render initial test image

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,15 @@
 console.log("hello world");
 
-require("dotenv").config();
+// mock db
+const imageLink = "../src/91qfVY7.jpeg";
+
+// DOM pieces
+const mainDiv = document.getElementById("main");
+const img = document.querySelector("img");
+
+function loadImage(url) {
+  img.src = url;
+}
+
+loadImage(imageLink);
+console.log("loaded image.(?)");
